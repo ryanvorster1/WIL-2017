2b59e7746d9a6564a6115fe7758c37f19bdb13db
Geimaj <16000925@vcconnect.co.za>
2017-10-18 22:13:38 +0200

    create solution with dll

---
+++ WIL/SystemLogic/Truck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemLogic
+{
+    public class Truck
+    {
+        private int ID;
+        private string vin;
+        private string reg;
+        private int kms;
+        private bool availible;
+
+
+        public Truck()
+        {
+
+        }
+
+    }
+
+   public class TruckType
+    {
+        private int ID;
+        private string type;
+        private string manufacturor;
+        private int engineSize;
+        private int serviceInterval;
+        private int maxWeight;
+        private int maxVol;
+        
+        public TruckType()
+        {
+
+        } 
+
+    }
+}
---
+++ WIL/WIL/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemLogic;
+
+namespace WIL
+{
+    class TestClass
+    {
+        public static void Main(string[] args)
+        {
+            Truck t;
+            Console.WriteLine("test...");
+            Console.ReadLine();
+        }
+    }
+}
