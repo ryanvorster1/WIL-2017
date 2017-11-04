4d99440dddbb141ff3f9cfb18a936cc7533dbc0a
Dan <danhow17@gmail.com>
2017-11-04 21:33:54 +0200

    Add truck form update

---
+++ WIL/WIL/TruckForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WIL
+{
+    public partial class TruckForm : Form
+    {
+        public TruckForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addTruckBtn_Click(object sender, EventArgs e)
+        {
+            AddTruckForm atf = new AddTruckForm();
+            atf.ShowDialog();
+        }
+    }
+}
