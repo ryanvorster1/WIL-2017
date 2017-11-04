7ae7e4aced2b7bfaea64807b867473e7e118a342
Geimaj <16000925@vcconnect.co.za>
2017-11-04 22:01:42 +0200

    fix bug im ServiceForm

--- WIL/WIL/ServiceForm.cs
+++ WIL/WIL/ServiceForm.cs
@@ -32,8 +32,9 @@ namespace WIL
 
         private void ListBoxHandle()
         {
-            lvServiceList.Columns.Add("Truck #", 200);
-            lvServiceList.Columns.Add("Service #", 200);
+            lvServiceList.Columns.Add("Truck #", 50);
+            lvServiceList.Columns.Add("Truck Type", 200);
+            lvServiceList.Columns.Add("Service #", 800);
 
         }
 
@@ -62,15 +63,15 @@ namespace WIL
             foreach (Service serviceItem in results)
             {
                 string[] tRowData = new string[3];
-                tRowData[0] = $"{serviceItem.Truck.Type.Type} #{serviceItem.Truck.ID.ToString()}";
-
+                tRowData[0] = $"{serviceItem.Truck.ID.ToString()}";
+                tRowData[1] = serviceItem.Truck.Type.Type;
                 var services = dbm.GetServiceItems(serviceItem);
                 string ser = "";
                 foreach (var item in services)
                 {
                     ser += item.ServiceType.Job + ",";
                 }
-                tRowData[1] = ser;
+                tRowData[2] = ser;
 
                 InsertListBoxItem(tRowData);
             }
